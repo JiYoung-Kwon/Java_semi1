75b070f3efd966a8e50a1ccb6ceaf61f714bc19a
JiYoung-Kwon <rhakrywkd@naver.com>
2021-03-22 14:06:23 +0900

    CheckOut(대여) 데이터&컨트롤러 클래스 -- 수정 중 진행사항

---
+++ Semi1/src/Project/CheckOut.java
@@ -0,0 +1,41 @@
+//반납 기능 데이터 클래스
+
+package Project;
+
+public class CheckOut {
+
+	//필요데이터 : id, 대여일자, 대여자, 대여할 책
+	//대여자 : 고객 클래스의 id
+	//대여할 책 : 도서 클래스의 id
+	
+	int id;
+	String date;
+	String person;
+	String book;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public String getBook() {
+		return book;
+	}
+	public void setBook(String book) {
+		this.book = book;
+	}
+	
+}

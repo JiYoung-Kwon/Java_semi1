8bc11554c6362a00dca062ade997a6f6dfa0cfd4
Moonlight775 <78495852+Moonlight775@users.noreply.github.com>
2021-03-25 17:26:13 +0900

    Book Final
    Book 최종본

--- Semi1/src/Project/BookController.java
+++ Semi1/src/Project/BookController.java
@@ -9,7 +9,12 @@ public class BookController {
 	public String append(Book b) {
 		String msg = "데이터가 정상적으로 저장되었습니다.";
 		try {
-			MainData.addBook(b);
+			if((b.getTitle().length() ==0 || b.getAuthor().length() ==0 )) {
+				msg = "값을 입력해주세요.";
+			} else {
+				MainData.addBook(b);
+			}
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 			msg = "데이터 저장에 실패하였습니다.";
@@ -17,22 +22,50 @@ public class BookController {
 		return msg;
 	}
 	
-	public void update(int index ,String title, String author,String sort,String date) {  // 수정 메소드
-		MainData.getBooks().get(index).setTitle(title);         // 해당 인덱스의 객체 데이터를 입력 값으로 변경.
- 		MainData.getBooks().get(index).setAuthor(author);       
-		MainData.getBooks().get(index).setSort(sort);
-		MainData.getBooks().get(index).setDate(date.replace("일", ""));	
+	public String update(int index ,String title, String author,String sort,String date) {  // 수정 메소드
+		String msg = "데이터가 정상적으로 수정되었습니다.";
+		
+		try {
+			if (title.length() == 0 || author.length() ==0) {			
+				msg = "값을 입력해주세요.";
+			}else {
+					MainData.getBooks().get(index).setTitle(title);         // 해당 인덱스의 객체 데이터를 입력 값으로 변경.
+					MainData.getBooks().get(index).setAuthor(author);       
+					MainData.getBooks().get(index).setSort(sort);
+					MainData.getBooks().get(index).setDate(date.replace("일", ""));
+					
+					for(int i= 0; i<MainData.getCheckOutInfo().size(); i++) {
+						CheckOut c = MainData.getCheckOutInfo().get(i);
+						System.out.println(c.getBookNo());
+						System.out.println(index+1);
+						if(c.getBookNo().equals(Integer.toString(index+1)))
+						{
+							System.out.println("수정함");
+							c.setTitle(title);
+							}
+					}		
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			msg = "데이터 수정이 중 오류가 발생하였습니다.";
+		}
+		
+		return msg;
 	}
 	
-	public String delete(Book b) {
+	public String delete(String title) {
 		String msg = "데이터가 정상적으로 삭제되었습니다.";
 		try {
-			for(int index=MainData.getBooks().size()-1; index>=0; index--) {
-				Book target = MainData.getBooks().get(index);
-				
-				if(target.getTitle().equals(b.getTitle())) {
-					MainData.getBooks().remove(index);
-					break;
+			if(title.length() == 0)
+				msg = "값을 선택해주세요.";
+			else {
+				for(int index=MainData.getBooks().size()-1; index>=0; index--) {
+					Book target = MainData.getBooks().get(index);
+					
+					if(target.getTitle().equals(title)) {
+						MainData.getBooks().remove(index);
+						break;
+					}
 				}
 			}
 		}catch(Exception e) {
@@ -54,16 +87,16 @@ public class BookController {
 	}
 	
 	public List<Book> search(String findStr, String findStr2){
-		List<Book> returnList = new ArrayList<Book>();
+		List<Book> returnList = new ArrayList<Book>();   // 반환해줄 리스트 선언
 		
 		
-		for(int i=0; i<MainData.getBooks().size(); i++) {
-			if(findStr.length()==0) {                 // 도서명(or No.)의 검색어가 없는경우
+		for(int i=0; i<MainData.getBooks().size(); i++) { //전체 내에서
+			if(findStr.length()==0) {                 // 도서명의 검색어가 없는경우
 				if(MainData.getBooks().get(i).getAuthor().indexOf(findStr2)>-1) {       // 저자명과 일치하는 객체만 추가.
 					returnList.add(MainData.getBooks().get(i));}
 				    }
 			else if (findStr2.length()==0) {          // 저자명의 검색어가 없는경우
-				if(MainData.getBooks().get(i).getTitle().indexOf(findStr)>-1) {         // 도서명(or No.)과 일치하는 객체만 추가
+				if(MainData.getBooks().get(i).getTitle().indexOf(findStr)>-1) {         // 도서명과 일치하는 객체만 추가
 					returnList.add(MainData.getBooks().get(i));}
 				}
 			else {if(MainData.getBooks().get(i).getTitle().indexOf(findStr)>-1&&   // 둘다 검색한경우.
@@ -98,5 +131,16 @@ public class BookController {
 		}
 		return index;
 	}
+	
+	List<Book> searchTitle(String title) {
+	      List<Book> resultList = new ArrayList<Book>();
+	      
+	      for(int i=0; i<MainData.getBooks().size(); i++) {
+	         if(MainData.getBooks().get(i).getTitle().contains(title))
+	            resultList.add(MainData.getBooks().get(i));            
+	      }
+	      
+	      return resultList;
+	   }
 	
 }
